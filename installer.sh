bf9bf127994127c70d9bdd44d1480e9c2d526d33
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2022-03-07 17:38:07 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ read key
 if [ "$key" = '1' ]; then
     git clone https://www.github.com/JerryBerry12/power-essesntials.git
     echo "alias jscmd='cd; cd power-essesntials; bash load_cmd.sh'" >> ~/.bashrc
-    echo "alias uninstall_jscmd='bash uninstall.sh'" >> ~/.bashrc
+    echo "alias uninstall_jscmd='cd ./power-essesntials; bash uninstall.sh'" >> ~/.bashrc
     echo "You have installed JS CMD! To test, type jscmd. To uninstall type uninstall_jscmd."
     source .bashrc
 
