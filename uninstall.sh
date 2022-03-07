bfcbba4abdbbbde381b36b52f3ddd60fca559e89
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2022-03-07 17:31:42 -0500

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+echo Uninstalling...
+cd
+rm -rf power-essesntials
+echo 'Uninstalled. Please note that aliases are still in session, please do not remove them. If you ever decide to reinstall, they are needed.'
