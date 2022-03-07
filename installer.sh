3848c27864d4217827967af260dcb4356f860954
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2022-03-07 17:14:44 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,6 @@
+cd
+git clone https://www.github.com/JerryBerry12/power-essesntials.git
+echo "alias jscmd='cd; cd power-essesntials; bash load_cmd.sh'" >> ~/.bashrc
+echo "alias uninstall_jscmd='bash uninstall.sh'" >> ~/.bashrc
+echo "You have installed JS CMD! To test, type jscmd. To uninstall type uninstall_jscmd.
+
